94f261c473bbac253809e894a184cc86c410914c
K1ller <k1ller@pop-os.localdomain>
2023-03-13 21:14:09 -0300

    first-commit

---
+++ app/classes/Routes.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace app\classes;
+
+class Routes {
+    public static function load($routes, $uri) {
+        
+        if (!array_key_exists($uri, $routes)) {
+            throw new \Exception("Rota não encontrada {$uri}");
+        }
+        
+        
+        return "../app/{$routes[$uri]}";
+    }
+}
+
